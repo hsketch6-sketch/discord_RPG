4690ca04ba3ef0a1d4ca398f05c3374b8c0a7068
hsketch6-sketch <hsketch6@gmail.com>
2026-04-22 22:18:05 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -170,8 +170,8 @@ int main() {
 	system("chcp 65001");
 	srand(static_cast<unsigned int>(time(NULL)));
 
-	// 2. 봇 객체 생성 (토큰을 꼭 정확히 입력하세요!)
-	dpp::cluster bot("");
+dpp::cluster bot(std::getenv("BOT_TOKEN")); 
+
 
 	// 3. 로그 활성화 (에러 확인용)
 	bot.on_log(dpp::utility::cout_logger());
